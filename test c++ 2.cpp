50c7591193ddd06deba39ab7d31002e92c3e9f11
Hetvi16-05 <103765741+Hetvi16-05@users.noreply.github.com>
2022-04-15 23:21:08 +0530

    Add files via upload

---
+++ test c++ 2.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+/* run this program using the console pauseror add your own getch ,system("pause") pr input loop */
+int main()
+{
+    std::cout<<"Helo World!";
+    return 0
+}
